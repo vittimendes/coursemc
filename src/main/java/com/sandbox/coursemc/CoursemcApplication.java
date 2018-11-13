209fe3606e1a07f313cafb222f03a0287842ea6e
vittimendes <vitormendes@gmail.com>
2018-11-13 11:09:37 -0200

    Projeto Criado

---
+++ src/main/java/com/sandbox/coursemc/CoursemcApplication.java
@@ -0,0 +1,12 @@
+package com.sandbox.coursemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemcApplication.class, args);
+	}
+}
