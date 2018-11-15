37ec7bce5bd5f56a3c94f192496a16eb870dcbc8
vittimendes <vitormendes@gmail.com>
2018-11-15 08:40:45 -0200

    insert db

--- src/main/java/com/sandbox/coursemc/CoursemcApplication.java
+++ src/main/java/com/sandbox/coursemc/CoursemcApplication.java
@@ -1,12 +1,33 @@
 package com.sandbox.coursemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sandbox.coursemc.domain.Category;
+import com.sandbox.coursemc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CoursemcApplication {
+public class CoursemcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Categoria 1");
+		Category cat2 = new Category(null, "Categoria 2");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 }
