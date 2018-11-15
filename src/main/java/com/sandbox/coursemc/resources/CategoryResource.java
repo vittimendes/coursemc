f2d3cffaa37278e9f2b5a89cc0d54a7b340f514f
vittimendes <vitormendes@gmail.com>
2018-11-14 23:46:54 -0200

    First domain class

--- src/main/java/com/sandbox/coursemc/resources/CategoryResource.java
+++ src/main/java/com/sandbox/coursemc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.sandbox.coursemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sandbox.coursemc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST funciona!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritorio");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
