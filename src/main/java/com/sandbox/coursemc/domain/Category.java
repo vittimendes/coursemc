ab6ead92c31c5a4f27cfc5a14517c98659e028d0
vittimendes <vitormendes@gmail.com>
2018-11-14 23:58:28 -0200

    BD h2 e Auto Create

--- src/main/java/com/sandbox/coursemc/domain/Category.java
+++ src/main/java/com/sandbox/coursemc/domain/Category.java
@@ -2,10 +2,18 @@ package com.sandbox.coursemc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;	
 	private String nome;
 	
 	public Category () {
